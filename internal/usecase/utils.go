3aa71cc0f94b6f0fdc526ef5eb33a5f47f618917
Max Yong <yongxc99@gmail.com>
2024-07-14 16:28:24 -0400

    fix cursor issue

--- internal/usecase/utils.go
+++ internal/usecase/utils.go
@@ -6,7 +6,13 @@ import (
 	"time"
 )
 
-func encodeCursor(cursor time.Time) string {
+func encodeCursor(cursor *time.Time) string {
+	if cursor == nil {
+		return ""
+	}
+	if cursor.IsZero() {
+		return ""
+	}
 	serializedCursor, err := json.Marshal(cursor)
 	if err != nil {
 		return ""
