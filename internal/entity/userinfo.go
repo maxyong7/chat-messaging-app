4386361d4d0a17143dfba9f40fda6b302655e469
Max Yong <yongxc99@gmail.com>
2024-07-21 17:03:51 -0400

    implement entity object for userprofile

---
+++ internal/entity/userinfo.go
@@ -0,0 +1,27 @@
+package entity
+
+type UserInfoDTO struct {
+	UserUUID  string
+	FirstName string
+	LastName  string
+	Avatar    string
+}
+
+type UserInfo struct {
+	UserUUID  string `json:"user_uuid,omitempty"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Avatar    string `json:"avatar"`
+}
+
+func (dto *UserInfoDTO) ToUserInfo() UserInfo {
+	if dto == nil {
+		return UserInfo{}
+	}
+	return UserInfo{
+		UserUUID:  dto.UserUUID,
+		FirstName: dto.FirstName,
+		LastName:  dto.LastName,
+		Avatar:    dto.Avatar,
+	}
+}
