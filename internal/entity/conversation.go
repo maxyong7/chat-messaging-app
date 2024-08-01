fa5c5ac36ff31f4c5cda0ebe164201aafbd4a381
Max Yong <yongxc99@gmail.com>
2024-08-01 17:34:14 +0800

    from conversations from inbox to conversation

--- internal/entity/conversation.go
+++ internal/entity/conversation.go
@@ -17,3 +17,11 @@ type ConversationDTO struct {
 	Content          string
 	CreatedAt        time.Time
 }
+
+type Conversations struct {
+	Title                *string    `json:"title"`
+	LastMessage          *string    `json:"last_message"`
+	LastSentUser         UserInfo   `json:"last_sent_user"`
+	LastMessageCreatedAt *time.Time `json:"last_message_created_at"`
+	Type                 *string    `json:"type"`
+}
